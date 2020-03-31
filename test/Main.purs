296b7f277d97dae6fda9977826ac1f9811660b82
Miles Frain <miles.frain@colorado.edu>
2020-03-31 17:37:32 -0600

    add Record tests

--- test/Main.purs
+++ test/Main.purs
@@ -3,10 +3,8 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
-import Test.Assert (assert)
+import Test.Record (runRecordTests)
 
 main :: Effect Unit
 main = do
-  log "Testing"
-  assert true
+  runRecordTests
