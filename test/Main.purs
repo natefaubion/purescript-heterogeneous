98dfc9a22436573c0cdbec94035f50b4380b80f4
Miles Frain <miles.frain@colorado.edu>
2020-03-31 20:11:11 -0600

    add HList tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,8 +5,10 @@ import Prelude
 import Effect (Effect)
 import Test.Record (runRecordTests)
 import Test.Variant (runVariantTests)
+import Test.HList (runHListTests)
 
 main :: Effect Unit
 main = do
   runRecordTests
   runVariantTests
+  runHListTests
